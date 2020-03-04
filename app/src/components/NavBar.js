8542eeeca59d929b2348ced0bb4cea942411f2f6
Eric Nunn <enunn@nexient.com>
2020-03-04 13:11:01 -0500

    added node-sass dependency. imported design pic. Setting up components folder

---
+++ app/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='nav'>
+          <ul>
+            <a href='#'><li>Home</li></a>    
+            <a href='#'><li>Gadgets</li></a>    
+            <a href='#'><li>Skills</li></a>    
+            <a href='#'><li>Missons</li></a>    
+            <a href='#'><li>Collections</li></a>    
+            <a href='#'><li>Characters</li></a>    
+          </ul>            
+        </div>
+    )
+}
+
+export default Navbar;
